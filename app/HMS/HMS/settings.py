d0b3c1723692de138b6b0e73dfdc0831a07b078b
petwo15 <162693908+petwo15@users.noreply.github.com>
2024-04-26 18:34:42 +0300

    Update settings.py

--- app/HMS/HMS/settings.py
+++ app/HMS/HMS/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "HMS.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mariadb",
+        "NAME": "HMS",
+        "USER": "root",
+        "PASSWORD": "P@ssw0rd#123456",
+        "HOST": "0.0.0.0",
+        "PORT": "3306"
     }
 }
 
